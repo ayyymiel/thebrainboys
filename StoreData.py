ab95f86c842abf6161bef462bad2535785a2bfe5
Ayodeji Onipe <ayosemail@gmail.com>
2021-09-04 21:29:10 -0400

    Updated: Trimmed Data @ 90 runs

--- StoreData.py
+++ StoreData.py
@@ -55,7 +55,7 @@ def main():
 
     board = BoardShim(args.board_id, params)
 
-    ACTION = 'other'  # THIS IS THE ACTION YOU'RE THINKING
+    ACTION = 'right'  # THIS IS THE ACTION YOU'RE THINKING
 
     board.prepare_session()
 

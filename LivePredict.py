0b8ad41de986eb3d13e36ca947d383f8e42a16b0
Ayodeji Onipe <ayosemail@gmail.com>
2021-09-15 20:46:19 -0400

    Updated: TrimmedData @ 140

--- LivePredict.py
+++ LivePredict.py
@@ -1,11 +1,13 @@
 import argparse
 import time
 import numpy as np
-import serial
+# import serial
+from pathlib import Path
 
 import brainflow
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+from numpy.core.numeric import NaN
 
 import tensorflow as tf
 from tensorflow.keras.models import Model
@@ -15,7 +17,7 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv1D,
 import pickle
 
 action = ["Backward", "Forward", "Left", "Other", "Right"]
-ser = serial.Serial("COM4", 9600, timeout = 1) #Change your port name COM... and your baudrate
+# ser = serial.Serial("COM4", 9600, timeout = 1) #Change your port name COM... and your baudrate
 BoardShim.enable_dev_board_logger()
 
 parser = argparse.ArgumentParser()
@@ -26,7 +28,7 @@ parser.add_argument('--ip-port', type=int, help='ip port', required=False, defau
 parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
                     default=0)
 parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
-parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='COM8')
+parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='COM6')
 parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
 parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
 parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
@@ -63,20 +65,30 @@ def main():
     data = data[1:9, 1:701] #reshaping data into same format
     data = np.array(data).reshape(-1, 8, 700)
 
-    model = tf.keras.models.load_model("CNN-30+%.model")
+    model = tf.keras.models.load_model(Path("Models\CNN80tanhActivation.model"))
 
     prediction = model.predict(data)
 
-    counter =-1
+    x = NaN
 
+    counter =-1
+    temp=0
+    highest=0
     for ACTION in np.nditer(prediction):
         counter+=1
-        if ACTION ==1.0:
-            x=counter
+        if counter==0:
+            temp=ACTION
+            highest=ACTION
+        elif counter>=1:
+            temp=ACTION
+            if temp>highest:
+                highest=temp
+                x=counter
 
     print("The action you are thinking is: ", prediction)
     print("The action you are thinking is: ", action[x])
 
+'''
     data = ''
     if x==  0:
         ser.write(b'b')
@@ -88,8 +100,9 @@ def main():
         ser.write(b'r')
     else:
         ser.write(b'o')
+        '''
 
 if __name__ == "__main__":
-    while(True==True):
-        main()
+    # while(True==True):
+    main()
 

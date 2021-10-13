7520584adb17edbd874224a24e8bd9f7a113b920
Amiel? <86252345+ayyymiel@users.noreply.github.com>
2021-10-12 21:17:16 -0400

    added accuracy filter and csv with accuracies > 0.75

---
+++ accuracy_filter.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+get_accuracies = pd.read_csv('accuracy_test.csv')
+low_end = []
+for i in range(len(get_accuracies)):
+    if get_accuracies.iloc[i, 0] < 0.75:
+        low_end.append(i)
+print(low_end)
+
+for row in low_end:
+    get_accuracies.drop(row, inplace=True)
+    print(f'Deleted: {row}')
+    get_accuracies.to_csv('new_acc.csv')
+        # new = get_accuracies.drop(i, axis=0, inplace=False)
+        # print(f'Deleted row {i}')
+        # print(new)
